c9334e8fbcdd5dedd05c311c4443f4b3495efe20
tarkIlJun <tack89826@gmail.com>
2023-08-31 18:36:18 +0900

    serviceImpl

---
+++ src/main/java/com/example/boardtest/service/BoardServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.boardtest.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.boardtest.dto.BoardDTO;
+import com.example.boardtest.mappers.BoardMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    ///////////////////////////////////////////////////
+    private final BoardMapper boardMapper;
+    
+    ///////////////////////////////////////////////////
+    @Override
+    public List<BoardDTO> boardList() {
+        
+        List<BoardDTO> list = boardMapper.boardList();
+
+        return list;
+    }
+
+    ///////////////////////////////////////////////////
+    @Override
+    public BoardDTO boardOne(Long bno) {
+        
+        BoardDTO boardOne = boardMapper.boardOne(bno);
+
+        return boardOne;
+    }
+    
+}
